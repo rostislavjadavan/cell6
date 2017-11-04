2c20998077fd442011a08e60bbc8dc4287c27c00
Rostislav Jadavan <rostislav.jadavan@gmail.com>
2017-11-04 22:33:02 +0100

    database

--- app/config.php
+++ app/config.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * Database
+ *
+ */
+$database = $container->singleton("\Core\Database");
+$database->setDb('pdosqlite://localhost/'.SYSPATH.DIRECTORY_SEPARATOR.'database/database.sqlite');
+
+/**
  * Routes
  * 
  */
@@ -13,8 +20,9 @@ $router->get('homepage', '/',  'Main::index');
 $router->get('page1', '/page1', 'Main::page1');
 $router->get('page2', '/page2/<name>', 'Main::page2');
 $router->get('view', '/view', 'page');
-$router->get('func', '/func', function() {
-    return 'func';
+$router->get('func', '/func', function() use ($container, $database) {
+    $result = $database->from('test')->many();
+    return print_r($result, true);
 });
 $router->get('bootstrap4', '/boot4', 'Main::bootstrap');
 $router->rest("api-endpoint", '/api', 'Rest');
