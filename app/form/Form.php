63716f8a470442cfead7e3062f47499f172025f2
Rostislav Jadavan <rostislav.jadavan@gmail.com>
2018-03-06 21:42:00 +0100

    form deletion

--- app/form/Form.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: spool
- * Date: 14/01/2018
- * Time: 21:04
- */
--- app/form/LoginFormModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: spool
- * Date: 17/01/2018
- * Time: 20:48
- */
